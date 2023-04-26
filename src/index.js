3f6d202a4619fc0b71baffe0f6a77737f4850c10
Vivek Mishra <v.mishra@f5.com>
2023-04-26 16:18:41 +0530

    prevent code duplication

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import _ from "lodash";
-import printMe from "./print.js";
 
 function component() {
   const element = document.createElement("div");
@@ -7,11 +6,6 @@ function component() {
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  
-  btn.innerHTML = "Click me and check the console!";
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
 
   return element;
 }
