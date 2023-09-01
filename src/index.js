d72e0996feb2925540645bb3ee004123b71a1753
Vivek Mishra <v.mishra@f5.com>
2023-09-01 18:46:37 +0530

    added caching support

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import _ from "lodash";
+import Print from "./print";
 
 async function getComponent() {
   const element = document.createElement("div");
   const { default: _ } = await import("lodash");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.onclick = Print.bind(null, "Hello webpack!");
 
   return element;
 }
