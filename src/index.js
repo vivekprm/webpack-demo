183eb64a6c2bbaa0a11e32d00e88f26b615800b8
Vivek Mishra <v.mishra@f5.com>
2023-09-06 17:07:45 +0530

    hot module reload in webpack

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 import _ from "lodash";
-import Print from "./print";
+import printMe from "./print.js";
+import "./styles.css";
 
-async function getComponent() {
+function component() {
   const element = document.createElement("div");
-  const { default: _ } = await import("lodash");
+  const btn = document.createElement("button");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.onclick = Print.bind(null, "Hello webpack!");
+
+  btn.innerHTML = "Click me and check the console!";
+  btn.onclick = printMe; // onclick event is bind to the original printMe function
+
+  element.appendChild(btn);
 
   return element;
 }
 
-getComponent().then((component) => {
-  document.body.appendChild(component);
-});
+let element = component();
+document.body.appendChild(element);
+
+if (module.hot) {
+  module.hot.accept("./print.js", function () {
+    console.log("Accepting the updated printMe module!");
+    document.body.removeChild(element);
+    element = component(); // Re-render the "component" to update the click handler
+    document.body.appendChild(element);
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -2,36 +2,30 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: {
-    index: "./src/index.js",
+    app: "./src/index.js",
   },
   devtool: "inline-source-map",
   devServer: {
     static: "./dist",
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Caching",
+      title: "Hot Module Replacement",
     }),
   ],
   output: {
-    filename: "[name].[contenthash].js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
-    clean: true, // clean up dist dir
-    publicPath: "/",
-  },
-  optimization: {
-    moduleIds: "deterministic",
-    runtimeChunk: "single",
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: "vendors",
-          chunks: "all",
-        },
-      },
-    },
+    clean: true,
   },
 };
